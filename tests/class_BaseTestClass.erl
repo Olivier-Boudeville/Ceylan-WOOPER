9d00ce314b498056e1c49813a44ba74bf931c235
Olivier Boudeville <ceylan-commits@esperide.com>
2018-12-22 09:29:48 +0100

    Updated with newer conventions.

--- tests/class_BaseTestClass.erl
+++ tests/class_BaseTestClass.erl
@@ -1,12 +1,30 @@
-% Copyright (C) 2003-2018 Olivier Boudeville
+% Copyright (C) 2014-2018 Olivier Boudeville
 %
-% This file is part of the Ceylan-WOOPER examples.
+% This file is part of the Ceylan-WOOPER library.
 %
-% It has been placed in the public domain.
+% This library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License or
+% the GNU General Public License, as they are published by the Free Software
+% Foundation, either version 3 of these Licenses, or (at your option)
+% any later version.
+% You can also redistribute it and/or modify it under the terms of the
+% Mozilla Public License, version 1.1 or later.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU Lesser General Public License and the GNU General Public License
+% for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License, of the GNU General Public License and of the Mozilla Public License
+% along with this library.
+% If not, see <http://www.gnu.org/licenses/> and
+% <http://www.mozilla.org/MPL/>.
 %
 % Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
-
-
+% Creation date: Wednesday, December 24, 2014
+%
 -module(class_BaseTestClass).
 
 
@@ -36,15 +54,7 @@ construct( State, Name, Gender ) ->
 	setAttributes( State, [ { name, Name }, { gender, Gender } ] ).
 
 
-% This useless destructor overriding was made to silence Dialyzer (which is not
-% able to determine that this function will never be called, as WOOPER performs
-% the appropriate test is made beforehand):
-%
-% Allows to test also the automatic destructor generation:
-%-spec destruct( wooper:state() ) -> wooper:state().
-destruct( State ) ->
-	State.
-
+% No specific destruct/1.
 
 
 % Method implementations.
@@ -71,7 +81,7 @@ setName( State, Name ) ->
 	wooper:return_state_only( NewState ).
 
 
-% Returns a mean count.
+% Returns some mean count.
 %
 % (static)
 %
