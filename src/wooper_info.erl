3ae32b5055ccf8d2d06ec9e8f5461310dee7495a
Olivier Boudeville <olivier.boudeville@esperide.com>
2018-12-27 10:41:10 +0100

    *_with became *_as.

--- src/wooper_info.erl
+++ src/wooper_info.erl
@@ -735,15 +735,15 @@ get_inner_builtins() ->
 	  {wooper_lookup_method,3},
 
 	  {wooper_execute_method,3},
-	  {wooper_execute_method_with,4},
+	  {wooper_execute_method_as,4},
 	  {wooper_effective_method_execution,4},
 
 	  {wooper_handle_local_request_execution,3},
-	  {wooper_handle_local_request_execution_with,4},
+	  {wooper_handle_local_request_execution_as,4},
 	  {wooper_handle_remote_request_execution,4},
 
 	  {wooper_handle_local_oneway_execution,3},
-	  {wooper_handle_local_oneway_execution_with,4},
+	  {wooper_handle_local_oneway_execution_as,4},
 	  {wooper_handle_remote_oneway_execution,3},
 
 	  {chain_parent_destructors,1},
