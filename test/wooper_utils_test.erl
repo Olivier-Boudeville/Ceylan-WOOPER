c2663000074e2b35f0082abac9b1a130a06dce79
Olivier Boudeville <olivier.boudeville@esperide.com>
2024-01-14 11:31:20 +0100

    Fixed copyright/creation dates.

--- test/wooper_utils_test.erl
+++ test/wooper_utils_test.erl
@@ -1,4 +1,4 @@
-% Copyright (C) 2003-2022 Olivier Boudeville
+% Copyright (C) 2008-2024 Olivier Boudeville
 %
 % This file is part of the Ceylan-WOOPER library.
 %
@@ -23,9 +23,10 @@
 % <http://www.mozilla.org/MPL/>.
 %
 % Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
+% Creation date: 2008.
 
 
-% Basic testing of the wooper_utils module.
+% @doc Basic testing of the wooper_utils module.
 -module(wooper_utils_test).
 
 
@@ -45,7 +46,7 @@ run() ->
 	FirstWOOPERClassname = 'class_BigPackage__MyPackage__MyExample',
 
 	FirstJavaFullClassname = wooper_utils:get_java_package_and_class_for(
-							   FirstWOOPERClassname   ),
+		FirstWOOPERClassname   ),
 
 	test_facilities:display( "To the WOOPER classname '~ts' corresponds "
 		"~p, i.e. ~ts.~n",
@@ -57,7 +58,7 @@ run() ->
 	SecondWOOPERClassname = 'class_MyPackage__MyExample',
 
 	SecondJavaFullClassname = wooper_utils:get_java_package_and_class_for(
-							   SecondWOOPERClassname   ),
+		SecondWOOPERClassname   ),
 
 	test_facilities:display( "To the WOOPER classname '~ts' corresponds "
 		"~p, i.e. ~ts.~n",
@@ -69,7 +70,7 @@ run() ->
 	ThirdWOOPERClassname = 'class_MyExample',
 
 	ThirdJavaFullClassname = wooper_utils:get_java_package_and_class_for(
-							   ThirdWOOPERClassname ),
+		ThirdWOOPERClassname ),
 
 	test_facilities:display( "To the WOOPER classname '~ts' corresponds "
 		"~p, i.e. ~ts.~n",
